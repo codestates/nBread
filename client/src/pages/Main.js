822f830311050a3e3b3747ae605e0aff5403ba95
ace0507 <kysys0507@naver.com>
2022-03-24 09:21:20 +0900

    Design: 모달
    로그인, 글쓰기 (채팅, 비밀번호찾기)진행중

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -3,6 +3,9 @@ import Navbar from '../component/Navbar'
 import styled from 'styled-components'
 import PostList from '../component/PostList';
 import PostDetail from "../component/PostDetail";
+import PostingWrite from "../modal/PostingWrite";
+import Chatting from "../modal/Chatting";
+
 
 const options = {
   //지도 생성 기본 옵션
@@ -12,7 +15,34 @@ const options = {
 
 
 function Main() {
+  //로그인 모달
+  const [PostingWriteModal, setPostingWriteModal] = useState(false);
+  const [ChattingModal, setChattingModal] = useState(false);
+  
+
+   //로그인 모달
+  const openModalPostingWrite = () => {
+    setPostingWriteModal(!PostingWriteModal);
+  }
+
+  const openModalChatting = () => {
+    setChattingModal(!ChattingModal)
+  }
+
+  //모달창이 떳을때 뒷배경 어둡게
+  const ModalBackdrop = styled.div`
+  position: fixed;
+  z-index: 999;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background-color: rgba(0,0,0,0.4);
+  display: grid;
+  place-items: center;
+`;
   const MapContainer = useRef(null); //지도 영역 DOM 레퍼런스
+
 
   useEffect(() => {
     new window.kakao.maps.Map(MapContainer.current, options); //지도 생성, 객체 리턴
@@ -104,9 +134,25 @@ function Main() {
                 ref={MapContainer}
               ></div>
         </MapDiv>
-        <WritingButton>글쓰기</WritingButton>
-        <ChattingButton>채팅</ChattingButton>
+
+        {/* 글쓰기 버튼 */}
+        <WritingButton onClick={openModalPostingWrite}>글쓰기</WritingButton>
+        {/* 채팅 버튼 */}
+        <ChattingButton onClick={openModalChatting}>채팅</ChattingButton>
+
       </Wrapper>
+
+      {/* 글쓰기 Modal */}
+      {PostingWriteModal === true ? <ModalBackdrop onClick={openModalPostingWrite}>
+      <PostingWrite onClick={(e) => e.stopPropagation()}>
+      </PostingWrite>
+      </ModalBackdrop>:null}
+
+      {/* 채팅 Modal */}
+      {ChattingModal === true ? <ModalBackdrop onClick={openModalChatting}>
+      <Chatting onClick={(e) => e.stopPropagation()}>
+      </Chatting>
+      </ModalBackdrop>:null}      
     </div>
   );
 }
