4c8ebf178080e5ada2733832ea74a773284fe089
leejiwon <365support@naver.com>
2022-03-28 13:45:56 +0900

    Feat: 글쓰기 기능 수정
    - 위도 경도 추가전 일단 글목록 추가되도록 수정

--- client/src/redux/postWriting/action.js
+++ client/src/redux/postWriting/action.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 import { WRITING_POST_SUCCESS, WRITING_POST_FAILURE } from "./types";
+import { useHistory } from 'react-router-dom';
 
+// const history = useHistory();
+// history.push('/')
 
 const writingPostSuccess = (post) => {
+  console.log('writingSuccess',post)
   return {
     type : WRITING_POST_SUCCESS,
     payload : post
@@ -17,10 +21,27 @@ const writingPostFailure = (error) => {
 };
 
 export const writingPost = (post) => {
-  console.log('actionpost',post)
+  // console.log('actionPost',post.address)
   return (dispatch) => {
-    axios.post(`${process.env.REACT_APP_API_URL}/contents`)
-    .then(post => dispatch(writingPostSuccess(post)))
+    axios.post(`${process.env.REACT_APP_API_URL}/contents`, {
+      address: post.address,
+      body: post.body,
+      category_food: post.category_food,
+      delivery_fee: post.delivery_fee,
+      recruitment_personnel: post.delivery_fee,
+      restaurant_name: post.restaurant_name
+    }, {withCredentials: true})
+    .then(data => {
+      // const history = useHistory();
+      console.log('ressss',data.status)
+      if(data.status === 201){
+        dispatch(writingPostSuccess(post))
+        // history.push('/')
+        // alert('글이 성공적으로 작성되었습니다.');
+      }else{
+        console.log('글쓰기 실패')
+      }
+    })
     .catch(err=> dispatch(writingPostFailure(err)))
   }
 }
--- client/src/redux/posts/reducer.js
+++ client/src/redux/posts/reducer.js
@@ -1,10 +1,9 @@
 import { SHOW_POST_LIST } from "./types";
 
 
-const postInitialState = {
+export const postInitialState = {
   posts:[]
 }
-
 
 const postsReducer = (state=postInitialState, action) => {
   switch(action.type){
@@ -14,9 +13,23 @@ const postsReducer = (state=postInitialState, action) => {
         ...state, 
         posts: post,
       }
+    
     default: return state;
   }
 }
 
 // 글 생성 조회 수정 삭제 실패
 export default postsReducer;
+
+
+// const postsReducer = (state=postInitialState, action) => {
+//   switch(action.type){
+//     case SHOW_POST_LIST:
+//       let post = action.payload;
+//       return {
+//         ...state, 
+//         posts: post,
+//       }
+//     default: return state;
+//   }
+// }
