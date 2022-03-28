a43159ce65e6c474831584d9a18e87c5dd96f3e2
HeeJeYang <ghjadmb21@gmail.com>
2022-03-28 11:57:58 +0900

    Feat: 경도 위도 추가
    * 경도 위도 1차 테스트

--- server/controllers/board/correction.js
+++ server/controllers/board/correction.js
@@ -3,14 +3,14 @@ const { recruitment_content } = require('../../models');
 module.exports = async (req, res) => {
   console.log(req.params, req.query)
   const { contentId } = req.params;
-  const { address, food, recruitmentPersonnel, fee, restaurantName, body, closed } = req.body
+  const { address, category_food, recruitment_personnel, delivery_fee, restaurant_name, body, lat, lng } = req.body;
   if (!closed) {
     await recruitment_content.update({
       address: address,
-      category_food: food,
-      recruitment_personnel: recruitmentPersonnel,
-      delivery_fee: fee,
-      restaurant_name: restaurantName,
+      category_food: category_food,
+      recruitment_personnel: recruitment_personnel,
+      delivery_fee: delivery_fee,
+      restaurant_name: restaurant_name,
       body: body
     }, { where: {
       id: contentId
--- server/seeders/20220322022446-demoRecruitment_content.js
+++ server/seeders/20220322022446-demoRecruitment_content.js
@@ -5,23 +5,27 @@ module.exports = {
 
     await queryInterface.bulkInsert('recruitment_content', [{
       user_id: 1,
-      address: 'daejeon',
+      address: '서울특별시 서초구 서초동 서초대로 396',
       category_food: 1, 
       delivery_fee: 1000,
       recruitment_personnel: 2,
       created_at: new Date(),
-      restaurant_name: 'nene',
+      restaurant_name: '네네치킨',
       body: 'come and join with me!',
+      lat: "37.49654",
+      lng: "127.02476",
       closed: 1
     }, {
       user_id: 2,
-      address: 'seoul',
+      address: '서울특별시 서초구 서초대로74길 4',
       category_food: 3,
       delivery_fee: 3000,
       recruitment_personnel: 3,
       created_at: new Date(),
-      restaurant_name: '6th floor house',
-      body: 'i like meat',
+      restaurant_name: '도미노피자',
+      body: 'i like pizza',
+      lat: "37.49672",
+      lng: "127.02571",
       closed: 1
   }], {});
 
