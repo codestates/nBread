37fd11237fdd1d3d310af81a61871dee03f296ca
Ubuntu <ubuntu@ip-172-31-88-82.ec2.internal>
2022-03-29 06:53:44 +0000

    Deploy: 1차 중간 배포

--- client/src/redux/posts/actions.js
+++ client/src/redux/posts/actions.js
@@ -3,7 +3,7 @@ import { SHOW_POST_LIST , RESET_POST_LIST, DELETE_POST_LIST} from "./types";
 
 const showPostSuccess = (post) => {
   const posts = post.data.data
-  console.log('posts',post)
+ //  console.log('showPostSuccess console: ',post)
   return {
     type : SHOW_POST_LIST,
     payload : posts,
@@ -29,7 +29,7 @@ const showPostDelete = () => {
 }
 // { params: {contentId: location.state.contentId}}
 export const showPostUserDelete = (contentId) => {
-  console.log('contentId',contentId)
+  // console.log('contentId',contentId)
   return (dispatch) => {
     axios.delete(`${process.env.REACT_APP_API_URL}/contents/${contentId}`)
     .then(res => {
@@ -46,15 +46,17 @@ export const showPostUserDelete = (contentId) => {
 
 
 export const showPostList = (info) => {
-  // console.log('info',info)
-  // console.log( `${info.swLatLng.lat},${info.swLatLng.lng}`,
+ //  console.log('info',info)
+ // console.log( `${info.swLatLng.lat},${info.swLatLng.lng}`,
     // `${info.neLatLng.lat},${info.neLatLng.lng}`)
   return (dispatch) => {
     if(!info){
       dispatch(showPostReset())
     }else{
       axios.get(`${process.env.REACT_APP_API_URL}/contents?start=${info.swLatLng.lat},${info.swLatLng.lng}&end=${info.neLatLng.lat},${info.neLatLng.lng}`)
-      .then(post => dispatch(showPostSuccess(post)))
+      .then(post => {
+	 // console.log("showPostList console: ",post)
+	dispatch(showPostSuccess(post))})
       .catch(err=> console.log(err))
     }
   }
