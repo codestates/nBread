d0c5bcd62cebe8e74335754e8ab246d02cfb5738
HeeJeYang <ghjadmb21@gmail.com>
2022-04-01 18:09:08 +0900

    Docs: 미 로그인 시 인터페 개선

--- server/controllers/board/lookup.js
+++ server/controllers/board/lookup.js
@@ -4,12 +4,7 @@ const { isAuthorized } = require('../tokenFunctions')
 
 module.exports = async (req, res) => {
   // 임시 데이터, contentIdArr 받아올 방법 생각하기
-  const token = isAuthorized(req, res);
-  if (!token) {
-    // 토큰 없을 시 반환할 때 201?? 수정해야하나 여쭤보기
-    return res.status(204).send({ message: '권한 없음' })
-  }
-
+  
   const { start, end } = req.query;
   if (!start || !end) {
     return res.status(204).send({message: '자료 없음'});
@@ -20,17 +15,8 @@ module.exports = async (req, res) => {
   const contentsIdArr = [];
   const mycontentsArr = [];
 
-  await user_content.findAll({
-    where: {
-      user_id: token.id
-    },
-    attributes: ['recruitment_content_id']
-  })
-  .then (result => {
-    for (data of result) {
-      mycontentsArr.push(data.dataValues.recruitment_content_id)
-    }
-
+  const token = isAuthorized(req, res);
+  if (!token) {
     recruitment_content.findAll({
       where : {
         lat : {
@@ -55,15 +41,6 @@ module.exports = async (req, res) => {
     })
     .then(data => {
       for (i of data) {
-        if (mycontentsArr.includes(i.dataValues.id)) {
-          if (i.dataValues.user_id === token.id) {
-            i.dataValues.rel = '모집자'
-          } else {
-            i.dataValues.rel = '신청자'
-          }
-        } else {
-          i.dataValues.rel = ''
-        }
         delete i.dataValues.userId
         contents.push(i.dataValues)
         contentsIdArr.push(i.dataValues.id)
@@ -81,12 +58,87 @@ module.exports = async (req, res) => {
         for (let i = 0; i < result.length; i++) {
           contents[i]['content_count'] = result[i].dataValues.content_count
         }
+
+        console.log(contents)
         res.status(200).send({ data: contents, message: '글 조회 성공' });
       })
     })
-  })
   .catch(err => {
     console.log('boardDetailLookup error :', err);
     res.status(500).send({message: '서버 에러'});
   })
+  } else {
+
+    await user_content.findAll({
+      where: {
+        user_id: token.id
+      },
+      attributes: ['recruitment_content_id']
+    })
+    .then (result => {
+      for (data of result) {
+        mycontentsArr.push(data.dataValues.recruitment_content_id)
+      }
+
+      recruitment_content.findAll({
+        where : {
+          lat : {
+            [Op.and] : {
+              [Op.gt] : Number(startArr[0]),
+              [Op.lt] : Number(endArr[0])
+            }
+          },
+          lng : {
+            [Op.and] : {
+              [Op.gt] : Number(startArr[1]),
+              [Op.lt] : Number(endArr[1])
+            }
+          },
+          [Op.not] : [
+            { closed : 2 }
+          ]
+        },
+        order: [
+          ['id', 'DESC']
+      ]
+      })
+      .then(data => {
+        for (i of data) {
+          if (mycontentsArr.includes(i.dataValues.id)) {
+            if (i.dataValues.user_id === token.id) {
+              i.dataValues.rel = '모집자'
+            } else {
+              i.dataValues.rel = '신청자'
+            }
+          } else {
+            i.dataValues.rel = ''
+          }
+          delete i.dataValues.userId
+          contents.push(i.dataValues)
+          contentsIdArr.push(i.dataValues.id)
+        } 
+
+        user_content.findAll({
+          group: ['recruitment_content_id'],
+          order: [['recruitment_content_id', 'DESC']],
+          attributes: ['recruitment_content_id', [Sequelize.fn('COUNT', 'recruitment_content_id'), 'content_count']],
+          where: {
+            recruitment_content_id: contentsIdArr
+          }
+        })
+        .then(result => {
+          for (let i = 0; i < result.length; i++) {
+            contents[i]['content_count'] = result[i].dataValues.content_count
+          }
+
+          console.log(contents)
+          res.status(200).send({ data: contents, message: '글 조회 성공' });
+        })
+      })
+    })
+    .catch(err => {
+      console.log('boardDetailLookup error :', err);
+      res.status(500).send({message: '서버 에러'});
+    })
+  }
 };
