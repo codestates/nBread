d1d25326bd7b934c04cda9cb86c3312be83b7fb8
HeeJeYang <ghjadmb21@gmail.com>
2022-04-06 15:55:38 +0900

    Feat: https 배포를 위한 환경 설정

---
+++ scripts/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/ubuntu/im-sprint-practice-deploy/server
+
+export DATABASE_USERNAME=$(aws ssm get-parameters --region us-east-1 --names DATABASE_USERNAME --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region us-east-1 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_NAME=$(aws ssm get-parameters --region us-east-1 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region us-east-1 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+
+authbind --deep pm2 start app.js
