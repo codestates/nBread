7a745fd59cccf0c17e197ce4bb5cc3506e052306
ace0507 <kysys0507@naver.com>
2022-04-11 09:17:50 +0900

    Feat: css수정, 랜딩페이지

--- client/src/component/Landing/Section2.js
+++ client/src/component/Landing/Section2.js
@@ -1,147 +1,149 @@
-import React, { useEffect, useState } from 'react'
-import styled, {keyframes} from 'styled-components';
-import MapImg from './img/map.png';
-import Chicken from './img/1.png';
-import Pizza from './img/2.png';
-import Tteokbokki from './img/3.png';
-import JapaneseFood from './img/4.png';
-import Fastfood from './img/5.png';
-import LunchBox from './img/6.png';
-import Jajangmyeon from './img/7.png';
-import Pasta from './img/8.png';
-import Bossam from './img/9.png';
-import Dessert from './img/10.png';
-import KoreanFood from './img/11.png';
-import Asianfood from './img/12.png';
-import Ramen from './img/13.png';
-import Curry from './img/14.png';
-import Steak from './img/15.png';
-import Udon from './img/16.png';
-import Hotpot from './img/17.png';
-import Shrimp from './img/18.png';
-import Ice from './img/19.png';
-import Ricebowl from './img/20.png';
-import Fried from './img/21.png';
-import Champon from './img/22.png';
-import Rib from './img/23.png';
-import Takoyaki from './img/24.png';
+import React from "react";
+import styled, { keyframes } from "styled-components";
+import ChattImg from "./img/nBreadPosting.png";
+import { Slide, Zoom } from "react-awesome-reveal";
+import Takoyaki from "./img/24.png";
 
-
-function Section2() {
-  const Foodlist = [
-    {image : Chicken},
-    {image : Pizza},
-    {image : Tteokbokki},
-    {image : JapaneseFood},
-    {image : Fastfood},
-    {image : LunchBox},
-    {image : Jajangmyeon},
-    {image : Pasta},
-    {image : Bossam},
-    {image : Dessert},
-    {image : KoreanFood},
-    {image : Asianfood},
-    {image : Ramen},
-    {image : Curry},
-    {image : Steak},
-    {image : Udon},
-    {image : Hotpot},
-    {image : Shrimp},
-    {image : Ice},
-    {image : Ricebowl},
-    {image : Fried},
-    {image : Champon},
-    {image : Rib},
-    {image : Takoyaki}
-  ];
-  const [currentIndex, setCurrentIndex] = useState(0)
-  useEffect(() => {
-    
-  });
-  
-
+function Section3() {
   return (
     <div>
       <Wrapper>
-      <TextDiv>
-      <Text>
-      주문 메뉴가 일치한다면 
-      </Text>
-      <Text>
-      신청하기 버튼으로 참여
-      </Text>
-      </TextDiv>
-      <ImageDiv>
-        <img src={MapImg} alt="banner" />
-      </ImageDiv>
+        <TextDiv>
+          <Zoom triggerOnce={false} duration={1500}>
+            <Text>모집자와 주문 메뉴가 </Text>
+            <Text>같다면 신청하기 클릭!</Text>
+            {/* <FoodImgDiv>
+            <Img src={Takoyaki} />
+          </FoodImgDiv> */}
+          </Zoom>
+        </TextDiv>
+        {/* <Div> */}
+        <ImageDiv>
+          <Slide triggerOnce={false} direction={"right"} duration={1500}>
+            <ChattImage src={ChattImg} alt="banner" />
+          </Slide>
+        </ImageDiv>
+        {/* </Div> */}
       </Wrapper>
-      <FoodDiv>
-      <FoodImgDiv>
-        {Foodlist.map(el => <Img src={el.image} />)}
-      </FoodImgDiv>
-      </FoodDiv>
-
-
-      
-    
     </div>
   );
 }
 
-export default Section2;
+export default Section3;
 
 const slideUp = keyframes`
-  from {
+  0% {
+      transform: translateX(0%);
+  }
+  10% {
+      transform: translateX(10%);
+      transform: translateY(10%);
+  }
+  20% {
+      transform: translateX(20%);
+      transform: translateY(20%);
+  }
+  50% {
+      transform: translateX(50%);
+     
+  }
+  75% {
+      transform: translateX(75%);
+    
+  }
+  100% {
       transform: translateX(100%);
+      
   }
-  to {
-      transform: translateX(-100%);
-  }
-`
+`;
 
 export const Wrapper = styled.div`
-  background-color: #D5B483;
-  border-bottom: 1px solid ;
+  background-color: #d9c6ac;
+  /* border-bottom: 1px solid; */
   padding-top: 75px;
   height: 100vh;
   padding: 0 20px;
   align-items: center;
   display: flex;
-  
+  @media (max-width: 710px) {
+    display: block;
+  }
+  @media (max-width: 576px) {
+    display: block;
+  }
+  @media (max-width: 400px) {
+    display: block;
+  }
 `;
 
 export const TextDiv = styled.div`
   margin-left: 20%;
-  
+  @media (max-width: 710px) {
+    margin-left: -100px;
+  }
+  @media (max-width: 576px) {
+    margin-left: 17%;
+  }
+  @media (max-width: 400px) {
+    margin-left: 14%;
+  }
 `;
 
 export const Text = styled.div`
-  color: #ffffff;
+  color: #b51d29;
   font-size: 56px;
   font-weight: 700;
   margin-bottom: 5px;
-`;
-
-export const FoodDiv = styled.div`
-  background-color: #D5B483;
+  @media (max-width: 710px) {
+    font-size: 3rem;
+  }
+  @media (max-width: 576px) {
+    font-size: 1.7rem;
+    padding-top: 10px;
+  }
+  @media (max-width: 400px) {
+    font-size: 1.7rem;
+    padding-top: 10px;
+  }
 `;
 
 export const ImageDiv = styled.div`
-  width: 40%;
+  @media (max-width: 710px) {
+  }
+  @media (max-width: 576px) {
+  }
+  @media (max-width: 400px) {
+  }
+`;
+
+export const ChattImage = styled.img`
+  margin-left: 180px;
+  @media (max-width: 1100px) {
+    margin-left: 10px;
+  }
+  @media (max-width: 770px) {
+    margin-left: 10px;
+    display: block;
+  }
+  @media (max-width: 576px) {
+    margin-left: 2px;
+    display: block;
+  }
+  @media (max-width: 400px) {
+    margin-left: 7px;
+    width: 85vw;
+  }
 `;
 
 export const FoodImgDiv = styled.div`
-  background-color: #D5B483;
-  width: 100%;
-  height:100% ;
-  animation-duration: 25s;
+  animation-duration: 0.3s;
   animation-timing-function: ease-out;
   animation-name: ${slideUp};
   animation-fill-mode: forwards;
-  
 `;
 
 export const Img = styled.img`
-  width: 80px;
-  margin-left: 30px;
+  position: relative;
 `;
+
+export const Div = styled.div``;
